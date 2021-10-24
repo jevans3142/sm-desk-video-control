97bfe6503463b65624170ec43877645a6af0e691
John Evans <djjevans+github@googlemail.com>
2021-10-24 23:03:26 +0100

    Begin serial software
    Alternate version for connection to existing RS232 based VGA switcher if required

--- src/SMVideoController/SMVideoController.ino
+++ src/SMVideoController/SMVideoController.ino
@@ -1,8 +1,15 @@
 #include "SMVideoController.h"
 #include "SMVideoController_panels.h"
 
+#include <SoftwareSerial.h>
+
+SoftwareSerial switcherSerial(0, 1); // RX, TX
+
 void setup() {
  setupControlPanels();
+
+ // set the data rate for the SoftwareSerial port attached to the video switcher
+ switcherSerial.begin(9600);
 }
 
 void loop() {
