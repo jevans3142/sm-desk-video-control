ab5d35bbfe248907a7a1bcb25e689a689e152aa1
John Evans <djjevans+github@googlemail.com>
2021-10-24 23:38:36 +0100

    Basic connection to switcher
    Add structure to main loop to maintain a connection to the videohub over Ethernet

--- src/SMVideoController/SMVideoController.ino
+++ src/SMVideoController/SMVideoController.ino
@@ -1,10 +1,51 @@
 #include "SMVideoController.h"
 #include "SMVideoController_panels.h"
 
+#include <NativeEthernet.h>
+
+// CONFIG====== 
+IPAddress arduinoIp(192, 168, 1, 100); // Teensy 4.1 in SM desk
+byte arduinoMac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress videohubIp(192, 168, 1, 200); // Switcher in patchbay
+EthernetClient client;
+//=============
+
 void setup() {
  setupControlPanels();
+
+  // start the Ethernet connection:
+  Ethernet.begin(arduinoMac, arduinoIp);
+
+  // give the Ethernet PHY a second to initialize; seems to improve reliability
+  delay(1000);
 }
 
 void loop() {
-  
+  if (! client.connected()) {
+    Serial.println("Currently disconnected.");
+    client.stop();
+
+    delay(2500); // Give videohub time to boot/think
+
+    Serial.println("connecting...");
+
+    if (client.connect(videohubIp, 9990)) {
+      Serial.println("connected");
+    }
+    else {
+      // if we didn't get a connection to the server:
+      Serial.println("connection failed");
+    }
+  }
+  else {
+    // do stuff here
+    
+  }
+}
+
+
+void setTie(byte out, byte in) {
+  //Ethernet output for Blackmagic
+  String s1 = "VIDEO OUTPUT ROUTING:\n"+ String(out) + " " + String(in) + "\n\n";
+  client.print(s1);
 }
